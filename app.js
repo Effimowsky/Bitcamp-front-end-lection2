37356aa0aa275223fab15f8a8c374d4d9e55355d
Effimowsky <94548214+Effimowsky@users.noreply.github.com>
2023-07-05 22:16:07 +0400

    done homework 2

---
+++ app.js
@@ -0,0 +1,135 @@
+// 12.დაწერეთ პროგრამა, რათა შეამოწმოთ რიცხვი ლუწია თუ კენტი -
+// Write a program to check if a number is even or odd.
+
+// function checkEvenOrOdd(number) {
+//   if (number % 2 == 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// }
+
+// console.log(checkEvenOrOdd(1671));
+// console.log(checkEvenOrOdd(19));
+// console.log(checkEvenOrOdd(132));
+
+// 13.დაწერეთ პროგრამა, რათა შეამოწმოთ რიცხვი დადებითია, უარყოფითი თუ ნული.
+// Write a program to check if a number is positive, negative, or zero.
+
+// function checkNumber(number) {
+//   if (number > 0) {
+//     console.log("Number Is Positive");
+//   } else if (number < 0) {
+//     console.log("Number Is Negative");
+//   } else {
+//     console.log("Number Is Zero");
+//   }
+// }
+
+// checkNumber(15);
+// checkNumber(-1);
+// checkNumber(0);
+
+// 14.დაწერეთ პროგრამა, რომ იპოვოთ მაქსიმალური ორი რიცხვიდან.
+// Write a program to find the maximum of two numbers.
+
+// function findMaximum(num1, num2) {
+//   if (num1 > num2) {
+//     return "num1 is maximum number";
+//   } else {
+//     return "num2 is maximum number";
+//   }
+// }
+
+// console.log(findMaximum(15, 55));
+// console.log(findMaximum(99, 23));
+// console.log(findMaximum(154, 923));
+
+// 15.დაწერეთ პროგრამა, რომ იპოვოთ მინიმალური ორი რიცხვიდან.
+// Write a program to find the minimum of two numbers.
+
+// function findMinumum(num1, num2) {
+//   if (num1 < num2) {
+//     return "num1 is minimum number";
+//   } else {
+//     return "num2 is minimum number";
+//   }
+// }
+
+// console.log(findMinumum(33, 15));
+// console.log(findMinumum(122, 1521));
+// console.log(findMinumum(11, 111));
+
+// 16.დაწერეთ პროგრამა, რათა შეამოწმოთ არის თუ არა რიცხვი გასაყოფი სხვა რიცხვზე.
+// Write a program to check if a number is divisible by another number.
+
+// function checkDivisibility(number, divisor) {
+//   if (number % divisor === 0) {
+//     console.log(number + " is divisible by " + divisor);
+//   } else {
+//     console.log(number + " is not divisible by " + divisor);
+//   }
+// }
+
+// checkDivisibility(10, 2);
+// checkDivisibility(15, 4);
+// checkDivisibility(151, 42);
+
+// 17.დაწერეთ პროგრამა, რათა შეამოწმოთ სტრიქონი ცარიელია თუ არა.
+// Write a program to check if a string is empty or not.
+
+// function checkEmptyString(str) {
+//   if (str === "") {
+//     console.log("String Is Empty");
+//   } else {
+//     console.log("String Is Not Empty");
+//   }
+// }
+
+// console.log(checkEmptyString(""));
+// console.log(checkEmptyString("Hello"));
+
+// 18.დაწერეთ პროგრამა, რათა შეამოწმოთ, შეიცავს თუ არა სტრიქონი კონკრეტულ სიტყვას.
+// Write a program to check if a string contains a specific word.
+
+// function checkWordInString(str, word) {
+//   if (str.includes(word)) {
+//     console.log("The string contains the word: " + word);
+//   } else {
+//     console.log("The string does not contain the word: " + word);
+//   }
+// }
+// checkWordInString("Hello, world!", "world");
+// checkWordInString("Hello, world!", "foo");
+// checkWordInString("The quick brown fox", "quick");
+
+// 19.დაწერეთ პროგრამა რომელიც კონსოლში გამოპრინტავს რიცხვებს 0 დან 10 ის ჩათვლით.
+// Print the numbers from 1 to 10 in the console.
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// 20.დაწერეთ პროგრამა, რათა შეამოწმოთ რიცხვი მარტივია თუ არა.
+// Write a program to check if a number is prime or not.
+
+// function isPrime(number) {
+//   if (number <= 1) {
+//     return false;
+//   }
+
+//   for (var i = 2; i <= Math.sqrt(number); i++) {
+//     if (number % i === 0) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// var number = 7;
+// console.log(`Is ${number} prime? ${isPrime(number)}`);
+// var number = 17;
+// console.log(`Is ${number} prime? ${isPrime(number)}`);
+// var number = 52;
+// console.log(`Is ${number} prime? ${isPrime(number)}`);
