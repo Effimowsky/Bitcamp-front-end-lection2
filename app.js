8e1a10070b41d3f8a95a1a75094862933644ac63
Effimowsky <94548214+Effimowsky@users.noreply.github.com>
2023-07-09 19:13:38 +0400

    update app.js

--- app.js
+++ app.js
@@ -1,104 +1,81 @@
 // 12.დაწერეთ პროგრამა, რათა შეამოწმოთ რიცხვი ლუწია თუ კენტი -
 // Write a program to check if a number is even or odd.
 
-// function checkEvenOrOdd(number) {
-//   if (number % 2 == 0) {
-//     return "Even";
-//   } else {
-//     return "Odd";
-//   }
-// }
+// let number = 6;
 
-// console.log(checkEvenOrOdd(1671));
-// console.log(checkEvenOrOdd(19));
-// console.log(checkEvenOrOdd(132));
+// if (number % 2 == 0) {
+//   console.log("Even");
+// } else {
+//   console.log("Even");
+// }
 
 // 13.დაწერეთ პროგრამა, რათა შეამოწმოთ რიცხვი დადებითია, უარყოფითი თუ ნული.
 // Write a program to check if a number is positive, negative, or zero.
 
-// function checkNumber(number) {
-//   if (number > 0) {
-//     console.log("Number Is Positive");
-//   } else if (number < 0) {
-//     console.log("Number Is Negative");
-//   } else {
-//     console.log("Number Is Zero");
-//   }
-// }
+// let number = -1;
 
-// checkNumber(15);
-// checkNumber(-1);
-// checkNumber(0);
+// if (number > 0) {
+//   console.log("Number is positive");
+// } else if (number < 0) {
+//   console.log("Number is negative");
+// } else {
+//   console.log("Number is zero");
+// }
 
 // 14.დაწერეთ პროგრამა, რომ იპოვოთ მაქსიმალური ორი რიცხვიდან.
 // Write a program to find the maximum of two numbers.
 
-// function findMaximum(num1, num2) {
-//   if (num1 > num2) {
-//     return "num1 is maximum number";
-//   } else {
-//     return "num2 is maximum number";
-//   }
-// }
+// let number1 = 15;
+// let number2 = 10;
 
-// console.log(findMaximum(15, 55));
-// console.log(findMaximum(99, 23));
-// console.log(findMaximum(154, 923));
+// if (number1 > number2) {
+//   console.log("Number1 is maximum");
+// } else {
+//   console.log("Number2 is maximum");
+// }
 
 // 15.დაწერეთ პროგრამა, რომ იპოვოთ მინიმალური ორი რიცხვიდან.
 // Write a program to find the minimum of two numbers.
 
-// function findMinumum(num1, num2) {
-//   if (num1 < num2) {
-//     return "num1 is minimum number";
-//   } else {
-//     return "num2 is minimum number";
-//   }
-// }
+// let number1 = 15;
+// let number2 = 110;
 
-// console.log(findMinumum(33, 15));
-// console.log(findMinumum(122, 1521));
-// console.log(findMinumum(11, 111));
+// if (number1 < number2) {
+//   console.log("Number1 is minimum");
+// } else {
+//   console.log("Number2 is minimum");
+// }
 
 // 16.დაწერეთ პროგრამა, რათა შეამოწმოთ არის თუ არა რიცხვი გასაყოფი სხვა რიცხვზე.
 // Write a program to check if a number is divisible by another number.
 
-// function checkDivisibility(number, divisor) {
-//   if (number % divisor === 0) {
-//     console.log(number + " is divisible by " + divisor);
-//   } else {
-//     console.log(number + " is not divisible by " + divisor);
-//   }
-// }
+// let number = 10;
 
-// checkDivisibility(10, 2);
-// checkDivisibility(15, 4);
-// checkDivisibility(151, 42);
+// if (number % 2 == 0) {
+//   console.log("Number is divisible by another number");
+// } else {
+//   console.log("Number is not divisible by another number");
+// }
 
 // 17.დაწერეთ პროგრამა, რათა შეამოწმოთ სტრიქონი ცარიელია თუ არა.
 // Write a program to check if a string is empty or not.
 
-// function checkEmptyString(str) {
-//   if (str === "") {
-//     console.log("String Is Empty");
-//   } else {
-//     console.log("String Is Not Empty");
-//   }
-// }
+// let word = "Hello";
 
-// console.log(checkEmptyString(""));
-// console.log(checkEmptyString("Hello"));
+// if (word != "") {
+//   console.log("is not empty");
+// } else {
+//   console.log("empty");
+// }
 
 // 18.დაწერეთ პროგრამა, რათა შეამოწმოთ, შეიცავს თუ არა სტრიქონი კონკრეტულ სიტყვას.
 // Write a program to check if a string contains a specific word.
+// let word = "Hello World";
 
-// function checkWordInString(str, word) {
-//   if (str.includes(word)) {
-//     console.log("The string contains the word: " + word);
-//   } else {
-//     console.log("The string does not contain the word: " + word);
-//   }
+// let word = "Hello World and Be The Good programmer";
+
+// if (word.includes("Good")) {
+//   console.log("Contains");
+// } else {
+//   console.log("not contains");
 // }
-// checkWordInString("Hello, world!", "world");
-// checkWordInString("Hello, world!", "foo");
-// checkWordInString("The quick brown fox", "quick");
